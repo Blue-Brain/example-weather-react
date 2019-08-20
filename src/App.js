602baafcfa7fddac13ef36065d6be76837ec9048
Blue_Brain <89130460372yandex.ru>
2019-08-20 12:48:21 +0300

    create two components Form and Weather. In the Form component set API, add tag input and button. From App component send props 'weatherMethod' to Form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
 import Info from './components/Info'
+import Form from './components/Form'
+import Weather from './components/Weather'
 
-function App() {
-  return (
-    <div>
-      <Info />
-    </div>
-  );
-}
+const API_KEY = "bf68996d2a372114b8d51e7154fb28df";
 
+class App extends React.Component {
+
+  gettingWeather = async (e) => {   // e->от слова event.
+    e.preventDefault();             // данная строка говорит о том что мы должны уничтожить обыкновенное поведение страницы (перезагрузку). 
+    const city = e.target.elements.city.value;
+    const api_url = await fetch (
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    const data = await api_url.json();
+    console.log(data);
+  }
+
+  render () {
+      return (
+        <div>
+          <Info />
+          <Form weatherMethod={this.gettingWeather} />
+          <Weather />
+        </div>
+      );
+    }
+  }
 export default App;
